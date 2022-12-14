002df25095fc004e54912ed8db69b9d1fe018a1c
brt9029 <brt9029@gmail.com>
2022-12-13 19:20:28 -0800

    basic ball physics added

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,8 +9,14 @@ export default class Game extends Phaser.Scene
     {
         const ball = this.add.circle(400, 250, 10, 0xffffff, 1);
         this.physics.add.existing(ball);
+        ball.body.setBounce(1, 1);
 
         ball.body.setCollideWorldBounds(true, 1, 1);
-        ball.body.setVelocity(200, 200);
+        ball.body.setVelocity(-200, 0);
+
+        const paddleLeft = this.add.rectangle(50, 250, 10, 100, 0xffffff, 1);
+        this.physics.add.existing(paddleLeft, true);
+
+        this.physics.add.collider(paddleLeft, ball);
     }
 }
